eff37c4f20ca1725aec82a8ebeb0e14cff5c7025
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-07-04 03:26:02 +0300

    fixed

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace AlternatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = alternatingCharacters(s);
+
+                Console.WriteLine(result);
+            }
+
+        }
+
+        private static int alternatingCharacters(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if(s[i]==s[i+1]) 
+                {
+                    count++;
+                }
+
+            }
+            return count;
+        }
+    }
+}
